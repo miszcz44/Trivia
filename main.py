2e5632bdbf5373b08ad3c2336ccbaac53f3755c2
Eryk Bojaruniec <erykb412@wp.pl>
2022-05-27 12:03:27 +0200

    Dodano kolo ratunkowe odrzuc jedno z dwoch

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import json
 import random
 
 points = 0
-lifelines = {"1": "fifty-fifty", "2": "reject one of two"}
+lifelines = {"1": "fifty-fifty", "2": "reject-one-of-two"}
 used_lifelines = []
 
 
 
-def Print_question_and_possibilities_and_get_the_answer(category, question_numb, list_of_possible_answers):
+def Print_question_and_possibilities_and_get_the_answer(question_numb, list_of_possible_answers):
     print(category[question_numb]["pytanie"])
-    Print_user_possibilities(category, question_numb, list_of_possible_answers)
+    Print_user_possibilities(question_numb, list_of_possible_answers)
     user_answer = input("Your answer:")
     return user_answer
 
-def Print_user_possibilities(category, question_numb, list_of_possible_answers):
+def Print_user_possibilities(question_numb, list_of_possible_answers):
     for answer_letter in list_of_possible_answers:
         print(answer_letter + ": " + category[question_numb][answer_letter])
     if len(lifelines) > 0 and len(used_lifelines) == 0:
@@ -22,11 +22,11 @@ def Print_user_possibilities(category, question_numb, list_of_possible_answers):
         for lifeline in lifelines:
             print(str(lifeline) + ". " + lifelines[lifeline])
 
-def Fifty_fifty(category, question_numb, list_of_possible_answers):
-    Remove_two_wrong_answers(category, question_numb, list_of_possible_answers)
-    return Print_question_and_possibilities_and_get_the_answer(category, question_numb, list_of_possible_answers)
+def Fifty_fifty(question_numb, list_of_possible_answers):
+    Remove_two_wrong_answers(question_numb, list_of_possible_answers)
+    return Print_question_and_possibilities_and_get_the_answer(question_numb, list_of_possible_answers)
 
-def Remove_two_wrong_answers(category, question_numb, list_of_possible_answers_after_fifty_fifty):
+def Remove_two_wrong_answers(question_numb, list_of_possible_answers_after_fifty_fifty):
     list_of_possible_answers_after_fifty_fifty.remove(category[question_numb]["correct_answer"])
     wrong_answer = random.choice(list_of_possible_answers_after_fifty_fifty)
     list_of_possible_answers_after_fifty_fifty = [category[question_numb]["correct_answer"], wrong_answer]
@@ -34,29 +34,69 @@ def Remove_two_wrong_answers(category, question_numb, list_of_possible_answers_a
     return list_of_possible_answers_after_fifty_fifty
 
 def Update_lifelines():
-    for _ in range(len(used_lifelines)):
+    flag = 0
+    for _ in range(len(lifelines)):
         for key, value in lifelines.items():
             if(value == used_lifelines[0]):
                 temp_key = key
+                flag = 1
+                break
+    if(flag == 1):
         lifelines.pop(temp_key)
 
+def Reject_one_of_two_answers(list_of_possible_answers, question_numb):
+    lifeline_user_answers = Ask_for_two_answers(list_of_possible_answers)
+    lifeline_user_answers = Remove_one_of_two_answers(lifeline_user_answers, question_numb)
+    return lifeline_user_answers
 
-def Answering_mechanism(category):
+def Ask_for_two_answers(list_of_possible_answers):
+    print("Enter the answers you are curious about, one after another")
+    lifeline_user_answers = []
+    lifeline_user_answers.append(input())
+    while(lifeline_user_answers[0] not in list_of_possible_answers):
+        print("Enter one of the possible answers. Try again")
+        lifeline_user_answers[0] = input()
+    list_of_possible_answers.remove(lifeline_user_answers[0])
+    lifeline_user_answers.append(input())
+    while (lifeline_user_answers[1] not in list_of_possible_answers):
+        print("Enter one of the possible answers. Try again")
+        lifeline_user_answers[1] = input()
+    return lifeline_user_answers
+
+def Remove_one_of_two_answers(list_of_possible_answers, question_numb):
+    list_of_possible_answers_after_rejection = ["a", "b", "c", "d"]
+    if category[question_numb]["correct_answer"] in list_of_possible_answers:
+        for i in range(len(list_of_possible_answers)):
+            if(list_of_possible_answers[i] != category[question_numb]["correct_answer"]):
+                list_of_possible_answers_after_rejection.remove(list_of_possible_answers[i])
+    else:
+        random_answer_of_two = random.choice(list_of_possible_answers)
+        list_of_possible_answers_after_rejection.remove(random_answer_of_two)
+    list_of_possible_answers_after_rejection.sort()
+    return list_of_possible_answers_after_rejection
+
+
+
+def Answering_mechanism():
     global points
     global questions_count
     global used_lifelines
     list_of_possible_answers = ['a', 'b', 'c', 'd']
     questions_count += 1
     question_number = random.randint(0, len(category)-1)
-    user_answer = Print_question_and_possibilities_and_get_the_answer(category, question_number, list_of_possible_answers)
+    user_answer = Print_question_and_possibilities_and_get_the_answer(question_number, list_of_possible_answers)
     if(user_answer == "1"):
         used_lifelines.append("fifty-fifty")
-        list_of_possible_answers = Remove_two_wrong_answers(category, question_number, list_of_possible_answers)
-        user_answer = Print_question_and_possibilities_and_get_the_answer(category, question_number, list_of_possible_answers)
+        list_of_possible_answers = Remove_two_wrong_answers(question_number, list_of_possible_answers)
+        user_answer = Print_question_and_possibilities_and_get_the_answer(question_number, list_of_possible_answers)
+    elif(user_answer == "2"):
+        used_lifelines.append("reject-one-of-two")
+        list_of_possible_answers = Reject_one_of_two_answers(list_of_possible_answers, question_number)
+        user_answer = Print_question_and_possibilities_and_get_the_answer(question_number, list_of_possible_answers)
     while(user_answer not in list_of_possible_answers and (user_answer not in lifelines.keys() or len(used_lifelines) > 0)):
         if(len(lifelines) > 0 and len(used_lifelines) == 0): print("Choose one of possible answers or take a lifeline. Try again")
         else: print("Choose one of possible answers. Try again")
-        user_answer = Print_question_and_possibilities_and_get_the_answer(category, question_number, list_of_possible_answers)
+        user_answer = Print_question_and_possibilities_and_get_the_answer(question_number, list_of_possible_answers)
     if(user_answer == category[question_number]["correct_answer"]):
         print("Correct answer")
         points += 1
@@ -88,5 +128,5 @@ while(questions_count != 10):
     elif(user_category_choice == 3): category = history_questions
     elif(user_category_choice == 4): category = biology_questions
     elif(user_category_choice == 5): category = chemistry_questions
-    Answering_mechanism(category)
+    Answering_mechanism()
 print("The game is over, your score: " + str(points) + "/" + str(questions_count))
